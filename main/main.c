70d285ab86d4ca2bfaaf7d8c131690bd15076a29
Marten <me@marten.xyz>
2020-05-18 14:41:26 +0200

    Cleaned up main

--- main/main.c
+++ main/main.c
@@ -1,8 +1,8 @@
 #include "audio_renderer.h"
 #include "source.h"
 #include "source_sdcard.h"
-/* #include "source_tone.h" */
-/* #include "source_bluetooth.h" */
+#include "source_tone.h"
+#include "source_bluetooth.h"
 #include "pcm.h"
 #include "mixer.h"
 
@@ -33,39 +33,28 @@ esp_err_t app_main(void) {
         }
     };
 
-    /* uint8_t *data, *out; */
-    /* size_t bytes_read, upsampled_len; */
-    /* int highest_sample_rate, last_highest_sample_rate = 0; */
-
-    /* bool dma_cleared = false; */
-    /* bool running; */
-
     renderer_init(&renderer_config);
 
 #ifdef ENABLE_SDCARD
-    /* states[states_len] = source_sdcard_init(); */
-    /* mixer_add_source(states[states_len]); */
-    /* states_len++; */
     source_sdcard_init();
     source_sdcard_play_file("/sdcard/strobe.wav");
     /* source_sdcard_play_file("/sdcard/test.wav"); */
 #endif
 
 #ifdef ENABLE_BLUETOOTH
-    states[states_len] = source_bt_init();
-    mixer_add_source(states[states_len]);
-    states_len++;
+    source_bluetooth_init();
 #endif
 
 #ifdef ENABLE_TONE
-    states[states_len] = source_tone_init();
-    mixer_add_source(states[states_len]);
-    states_len++;
+    source_tone_init();
 
     vTaskDelay(15000/portTICK_PERIOD_MS);
-    source_tone_play_tone(SQUARE, 300, 10000, &renderer_config.pcm_format);
+    source_tone_play_tone(440, 10000);
 #endif
 
+    // Mixer takes data directly from the source ctxs list, and sends it to the
+    // renderer.
+    // TODO: Split this, so that the final data is available to other components.
     mixer_init();
 
     /*
@@ -75,61 +64,11 @@ esp_err_t app_main(void) {
     */
 
     /**
-     * Main audio loop. Retrieve data from audio source buffers,
-     * reformat it and send it to the mixer
+     * Is going to be used for things like a webserver / syncing other nodes
+     * Maybe.
      */
     for (;;) {
-        /* running = false; */
-
-        /**
-        for (int i = 0; i < states_len; i++) {
-
-            if (states[i]->status == PLAYING) {
-                running = true;
-
-                if (states[i]->buffer.format.sample_rate > highest_sample_rate) {
-                    highest_sample_rate = states[i]->buffer.format.sample_rate;
-                }
-
-                data = (uint8_t *)xRingbufferReceive(states[i]->buffer.data, &bytes_read, 0);
-                
-                if (bytes_read > 0 && data) {
-                    // Upsample everything (if SR is equal, data is being copied)
-                    // out = upsample(data, bytes_read, &upsampled_len, states[i]->buffer.format, highest_sample_rate);
-                    // TODO: Change bit depth
-                    // TODO: Send data to mixer
-
-                    // render_samples((int16_t *)out, upsampled_len);
-                    render_samples((int16_t *)data, bytes_read);
-                    vRingbufferReturnItem(states[i]->buffer.data, data);
-                    // free(out);
-                    dma_cleared = false;
-                }
-            }
-        }
-
-        if (highest_sample_rate != last_highest_sample_rate && highest_sample_rate > 0) {
-            renderer_set_sample_rate(highest_sample_rate);
-            last_highest_sample_rate = highest_sample_rate;
-        }
-
-        if (!dma_cleared && !running) {
-            renderer_clear_dma();
-            dma_cleared = true;
-        }
-
-        if (!running) 
-            vTaskDelay(pdMS_TO_TICKS(50));
-        **/
-
-        /* data = (uint8_t *)xRingbufferReceive(mixer_buffer, &bytes_read, 0); */
-        /* if (bytes_read > 0 && data) { */
-        /*     render_samples((int16_t *)data, bytes_read); */
-        /*     vRingbufferReturnItem(mixer_buffer, data); */
-        /*     dma_cleared = false; */
-        /* } else { */
-        /* } */
-            vTaskDelay(pdMS_TO_TICKS(500));
+            vTaskDelay(pdMS_TO_TICKS(10000));
     }
 
     return 0;
